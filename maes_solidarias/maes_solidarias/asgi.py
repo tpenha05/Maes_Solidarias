6a71b6669e3de315a71bd43b49321d8c566bf038
tpenha05 <thiagoppc@al.insper.edu.br>
2024-02-29 14:57:24 -0300

    projeto start e site rodando

---
+++ maes_solidarias/maes_solidarias/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for maes_solidarias project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maes_solidarias.settings')
+
+application = get_asgi_application()
