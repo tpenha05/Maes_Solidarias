d341a2a908490ef47948c8959fb4e3359648267f
WeeverAlex <awever4000@gmail.com>
2024-03-03 13:05:34 -0300

    commit criando pagina de doaçoes

---
+++ maes_solidarias/donations/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Donation
+
+class DonationForm(forms.ModelForm):
+    class Meta:
+        model = Donation
+        fields = ['name', 'email', 'phone_number', 'birth_date', 'cpf', 'amount', 'periodicity']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'amount': forms.RadioSelect(choices=Donation.AMOUNT_CHOICES),
+            'periodicity': forms.Select(),
+        }
