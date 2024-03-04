617f2fb7159ae96f84f12662fb8d4a8e7e459b64
DeenaElOrra <deenaeo@al.insper.edu.br>
2024-03-04 20:10:08 -0300

    models

--- maes_solidarias/ong/views.py
+++ maes_solidarias/ong/views.py
@@ -1,20 +1,10 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Doador
 
 def index(request):
     return redirect('accounts/login/')
 
-@login_required
-def profile(request):
-    doador = get_object_or_404(Doador, user=request.user)
-    
-    return render(request, 'profile.html', {'doador': doador})
-
-@login_required
-def create_profile(request):
-    if request.method == 'POST':
 
 
 
