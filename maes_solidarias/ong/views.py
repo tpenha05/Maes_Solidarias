66941ef298286165bb98866c956b2562de97755d
WeeverAlex <awever4000@gmail.com>
2024-03-04 23:39:49 -0300

    adicionando tela de inicio e colocando html bruto

--- maes_solidarias/ong/views.py
+++ maes_solidarias/ong/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 def index(request):
     return redirect('accounts/login/')
 
+def home(request):
+    return render(request, 'base.html')
+
 
 
 
