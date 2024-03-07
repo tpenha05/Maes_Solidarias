ddec33a83dab5840d7e198b5b9f1580662b4600a
WeeverAlex <awever4000@gmail.com>
2024-03-07 13:37:19 -0300

    colocando doacoes na home, e fazendo as campanhas

--- maes_solidarias/ong/urls.py
+++ maes_solidarias/ong/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('' , views.home, name='home'),
     path('add_campanha/', views.add_campanha, name='add_campanha'),
     path('add_evento/', views.add_evento, name='add_evento'),
-   
+    path('natal/', views.natal, name='natal'),
+    path('pascoa/', views.pascoa, name='pascoa'),
+    path('inverno/', views.inverno, name='inverno'),
+    path('dia-das-criancas/', views.criancas, name='criancas'),
 ]
